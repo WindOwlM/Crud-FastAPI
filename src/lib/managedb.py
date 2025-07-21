295eda002c024b8b95197efe2ca77923ea31f64f
Maicol <mamian642@gmail.com>
2025-07-21 16:15:54 -0500

    Fix: se arregla el acceso a la db

--- src/lib/managedb.py
+++ src/lib/managedb.py
@@ -1,13 +1,15 @@
 import pathlib
 import json
+from pathlib import Path
 
 class Managedb:
-    __address_file = "{0}/db/dbApp.json".format(pathlib.Path().absolute())
+    BASE_DIR = Path(__file__).resolve().parent.parent  # apunta a "src"
+    ADDRESS_FILE = BASE_DIR / "db" / "dbApp.json"
     
     def read_contacts(self):
-        with open(self.__address_file, mode= "r") as data:
+        with open(self.ADDRESS_FILE, mode= "r") as data:
             return json.loads(data.read())
 
     def write_contact(self,new_data):
-        with open(self.__address_file, "w") as data:
+        with open(self.ADDRESS_FILE, "w") as data:
             data.write(json.dumps(new_data))
