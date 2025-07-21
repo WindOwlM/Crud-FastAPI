b3f28e7d53a39f6419f1535671bd873223e868c9
Maicol <mamian642@gmail.com>
2025-07-21 16:07:20 -0500

    posiblke arreglo

--- src/lib/managedb.py
+++ src/lib/managedb.py
@@ -2,7 +2,7 @@ import pathlib
 import json
 
 class Managedb:
-    __address_file = "{0}\\src\\db\\dbApp.json".format(pathlib.Path().absolute())
+    __address_file = "{0}/db/dbApp.json".format(pathlib.Path().absolute())
     
     def read_contacts(self):
         with open(self.__address_file, mode= "r") as data:
